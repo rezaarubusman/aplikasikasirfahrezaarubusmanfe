1106f969e38b567896c7919faf022afbbf897924
Fahreza Aqsa Arubusman <fahreza.arubusman@gmail.com>
2026-06-25 16:16:11 +0700

    fix: perbaikan deskripsi reportproduct.tsx, favicon (#27)
    Co-authored-by: rezaarubusman <fahreza.arubusman@gmail.comgit config --global user.name rezaarubusmangit config --global user.email fahreza.arubusman@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,8 @@ export const links: Route.LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+    }, 
+    { rel: "icon", href: "/favicon-32x32.png", type: "image/svg+xml" },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
